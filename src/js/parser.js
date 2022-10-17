f05a1a2f1e3c9b78dd9d54b6e7e2f47da8069fe3
Ivan Nor <ivan.norik@gmail.com>
2022-10-17 20:49:45 +0600

    fix blank line in line 4

--- src/js/parser.js
+++ src/js/parser.js
@@ -1,7 +1,6 @@
 export default (stringContainingXMLSource) => {
   const parserInstanse = new DOMParser();
   const doc = parserInstanse.parseFromString(stringContainingXMLSource, 'application/xml');
-
   const items = doc.querySelectorAll('item');
   const channelTitle = doc.querySelector('channel > title');
   const channelDescription = doc.querySelector('channel > description');

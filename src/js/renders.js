c7146afc84e8f680c1477a346d031ab503a43d20
Ivan Nor <ivan.norik@gmail.com>
2022-10-18 22:01:40 +0600

    fix renderModal & renderContent

--- src/js/renders.js
+++ src/js/renders.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import { clickPostHandler } from './controllers';
 
 const createCardElement = (title) => {
   const card = document.createElement('div');
@@ -21,11 +22,11 @@ const createCardElement = (title) => {
   return card;
 };
 
-const renderModal = ({ title, description, pubDate, link, postId }) => (event) => {
-  event.preventDefault();
-  const modal = document.querySelector('.modal');
-  const modalTitle = document.querySelector('.modal-title')
-  console.log(modal, modalTitle, modalBody, modalA);
+export const renderModal = ({
+  title, description, link,
+}) => {
+  console.log('title', title, 'descrip', description, 'link', link);
+  const modalTitle = document.querySelector('.modal-title');
   modalTitle.textContent = '';
   const modalBody = document.querySelector('.modal-body');
   modalBody.textContent = '';
@@ -35,17 +36,12 @@ const renderModal = ({ title, description, pubDate, link, postId }) => (event) =
   modalTitle.textContent = title;
   modalBody.textContent = description;
   modalA.setAttribute('href', link);
+};
 
-  const a = event.target.parentNode.querySelector('a')
-  console.log(a);
-  a.className = 'fw-normal link-secondary';
-}
-
-export default (feedsElement, postsElement, channels) => {
+export const renderContent = (feedsElement, postsElement, channels) => {
+  console.log('RENDER CONTENT');
   feedsElement.innerHTML = '';
   postsElement.innerHTML = '';
-
-
 
   const feedsCard = createCardElement('Фиды');
   const feedsUl = feedsCard.querySelector('ul');
@@ -59,18 +55,20 @@ export default (feedsElement, postsElement, channels) => {
     const titleElement = document.createElement('h3');
     titleElement.classList.add('h6', 'm-0');
     titleElement.innerHTML = channelTitle;
-  
+
     const descriptionElement = document.createElement('p');
     descriptionElement.classList.add('m-0', 'small', 'text-black-50');
     descriptionElement.innerHTML = channelDescription;
-  
+
     const feedsLi = document.createElement('li');
     feedsLi.append(titleElement, descriptionElement);
     feedsLi.classList.add('list-group-item', 'border-0', 'border-end-0');
     feedsUl.append(feedsLi);
 
-    _.sortBy(posts, (post) => post.postId).forEach(({ title, description, pubDate, link, postId }) => {
-
+    _.sortBy(posts, (post) => post.postId).forEach((post) => {
+      const {
+        title, link, postId, visited,
+      } = post;
       const postsLi = document.createElement('li');
 
       const postA = document.createElement('a');
@@ -78,17 +76,18 @@ export default (feedsElement, postsElement, channels) => {
       postA.setAttribute('target', '_blank');
       postA.setAttribute('rel', 'noopener noreferrer');
       postA.setAttribute('data-id', postId);
-      postA.classList.add('fw-bold');
-      postA.textContent = description;
+      postA.className = visited ? 'fw-normal link-secondary' : 'fw-bold';
+      postA.textContent = title;
+      postA.addEventListener('click', clickPostHandler(post));
 
       const postButton = document.createElement('button');
       postButton.classList.add('btn', 'btn-outline-primary', 'btn-sm');
       postButton.setAttribute('data-id', postId);
       postButton.setAttribute('type', 'button');
-      postButton.setAttribute('data-bs-toggle', 'modal')
-      postButton.setAttribute('data-bs-target', '#exampleModal')
+      postButton.setAttribute('data-bs-toggle', 'modal');
+      postButton.setAttribute('data-bs-target', '#exampleModal');
       postButton.textContent = 'Просмотр';
-      postButton.addEventListener('click', renderModal({ title, description, pubDate, link, postId }));
+      postButton.addEventListener('click', clickPostHandler(post));
 
       postsLi.append(postA, postButton);
       postsLi.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0', 'mr-1');
@@ -101,4 +100,3 @@ export default (feedsElement, postsElement, channels) => {
     postsElement.append(postsCard);
   }
 };
-

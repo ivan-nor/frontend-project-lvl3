6bb01cf5b8d07acf97ce4351785f410097ab1336
Ivan Nor <ivan.norik@gmail.com>
2022-10-20 19:27:16 +0600

    add new param to validate url : not one of

--- src/js/validator.js
+++ src/js/validator.js
@@ -1,15 +1,16 @@
 import * as yup from 'yup';
 import keyBy from 'lodash/keyBy.js';
 
-const schema = yup.object().shape({
-  url: yup.string().url(),
-});
+export default (url, feeds) => {
+  const schema = yup.object().shape({
+    url: yup.string().url().notOneOf(feeds.map((feed) => feed.url)),
+  });
 
-export default (url) => {
   console.log('VALIDATOR, url --> ', url);
+  // const feedLinks = collection.map(({ link }) => link);
   try {
-    schema.validateSync({ url }, { abortEarly: false }); // isValidSync() --> boolean
-    return {}; // или можно validateSync() --> возвращает массив ошибок
+    schema.validateSync({ url }, { abortEarly: false });
+    return {}; // isValidSync() ---> boolean или можно validateSync() ----> возвращает массив ошибок
   } catch (e) {
     console.log('ERROR', keyBy(e.inner, 'path'));
     return keyBy(e.inner, 'path');

3f1469c5b5390a83bf80cdcee5f50142d6d7c1a7
Ivan Nor <ivan.norik@gmail.com>
2022-10-20 19:26:14 +0600

    fix state params from channels to feeds and posts & fix linter return on the end of arrow function

--- src/js/init.js
+++ src/js/init.js
@@ -5,39 +5,40 @@ import resources from '../locales/index.js';
 import { inputHandler, submitHandler } from './controllers';
 import setWatcher from './watcher';
 
-export default async (container, initialState = {}) => {
+export default (container, initialState = {}) => {
   const { lng = 'ru' } = initialState;
 
-  const i18nInstance = i18n.createInstance();
-  await i18nInstance.init({
-    lng,
-    resources,
+  i18n.createInstance({ lng, resources }, (err, t) => {
+    if (err) return console.log('something went wrong loading', err);
+
+    const state = {
+      process: 'input',
+      inputValue: '',
+      isValid: true,
+      errorMessages: {},
+      feeds: [],
+      posts: [],
+    };
+
+    console.log('INIT');
+    const watchedState = setWatcher(container, state, t);
+
+    const form = container.querySelector('form');
+    const input = form.querySelector('input');
+    // const button = form.querySelector('button');
+
+    const a = container.querySelector('#google'); // затычка для быстрого ввода ссылок
+    a.addEventListener('click', (event) => {
+      event.preventDefault();
+      state.inputValue = event.target.textContent;
+      input.value = event.target.textContent;
+    });
+
+    form.addEventListener('submit', submitHandler(watchedState));
+    input.addEventListener('input', inputHandler(watchedState));
+    input.focus();
+    // button.disabled = false;
+
+    return null; // требования линтера
   });
-
-  const state = {
-    process: 'input',
-    inputValue: '',
-    isValid: true,
-    errorMessages: {},
-    channels: [],
-  };
-
-  console.log('INIT');
-  const watchedState = setWatcher(container, state, i18nInstance);
-
-  const form = container.querySelector('form');
-  const input = form.querySelector('input');
-  const button = form.querySelector('button');
-
-  const a = container.querySelector('#google'); // затычка для быстрого ввода ссылок
-  a.addEventListener('click', (event) => {
-    event.preventDefault();
-    state.inputValue = event.target.textContent;
-    input.value = event.target.textContent;
-  });
-
-  form.addEventListener('submit', submitHandler(watchedState));
-  input.addEventListener('input', inputHandler(watchedState));
-  input.focus();
-  button.disabled = false;
 };

d352fa72d22f13b35b96d3c1e41366cc27a3cb5a
Ivan Nor <ivan.norik@gmail.com>
2022-10-22 10:29:32 +0600

    fix all texts

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -3,8 +3,21 @@ export default {
     languages: {
       ru: 'Русский',
     },
+    modal: {
+      link: 'Читать полностью',
+      button: 'Закрыть',
+    },
+    title: 'RSS агрегатор',
+    lead: 'Начните читать RSS сегодня! Это легко, это красиво.',
     submit: 'Добавить',
-    invalidFeedback: 'Пожалуйста, введите корректную ссылку.',
-    label: 'Введите rss-ссылку, например http://google.com',
+    label: 'Введите RSS-ссылку',
+    example: 'Пример: https://ru.hexlet.io/lessons.rss',
+    success: 'RSS успешно загружен',
+    errorMessages: {
+      networkError: 'Ресурс недоступен',
+      formatError: 'нет RSS по данной ссылке',
+      url: 'URL должен быть валидным url',
+      notOneOf: 'URL уже есть в списке',
+    },
   },
 };

5333a8231be6a52a1afeece51011acc43df3cd7b
Ivan Nor <ivan.norik@gmail.com>
2022-10-23 20:04:57 +0600

    fix proxy on disableCache = true

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,6 @@ import '../styles/style.scss';
 const point = document.querySelector('#point');
 
 const lng = 'ru';
-const proxy = 'https://allorigins.hexlet.app/get?url=';
+const proxy = 'https://allorigins.hexlet.app/get?disableCache=true&url=';
 
 init(point, { lng, proxy });

3268d5d119a51d0f694ee9b92111b6e396084125
Ivan Nor <ivan.norik@gmail.com>
2022-11-24 11:17:25 +0300

    add string JSON checking

--- src/js/parser.js
+++ src/js/parser.js
@@ -1,10 +1,20 @@
-export default (stringContainingXMLSource) => {
+const isJsonString = (str) => {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
+
+export default (str) => {
+  const stringContainingXMLSource = isJsonString(str) ? JSON.parse(str) : str;
   const parserInstanse = new DOMParser();
   const doc = parserInstanse.parseFromString(stringContainingXMLSource, 'application/xml');
   const items = doc.querySelectorAll('item');
   const channelTitle = doc.querySelector('channel > title');
   const channelDescription = doc.querySelector('channel > description');
-  // console.log('DOC', doc)
+  console.log('DOC', doc);
   const channel = {
     channelTitle: channelTitle.textContent,
     channelDescription: channelDescription.textContent,

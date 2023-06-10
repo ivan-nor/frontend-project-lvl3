a3e3717aadca0d710d3e3eaa796aa8f63a79718b
Ivan Nor <ivan.norik@gmail.com>
2023-06-10 15:02:45 +0300

    fix console logs & ignoreKeys

--- src/js/watcher.js
+++ src/js/watcher.js
@@ -1,47 +1,31 @@
 import onChange from 'on-change';
-// import { isEmpty } from 'lodash';
 import {
-  renderFeeds, renderPosts, renderMessage, renderForm,
+  renderFeeds, renderPosts, renderMessage, renderForm, renderModal,
 } from './renders';
 
 export default (elements, state, t) => {
-  const {
-    feeds,
-    posts,
-  } = elements;
-
-  console.log('SET WATCHER');
-  let counter = 0;
-
-  function watch(path, value, previousValue) {
-    counter += 1;
-    if (path !== 'inputValue') {
-      console.log('WATCHER CALLED', counter, 'PATH =', path, '\n   PREV  ->', previousValue, '\n   VALUE ->', value);
-    }
-
+  function watch(path) {
     switch (path) {
       case 'process':
         renderForm(state, elements, t);
         break;
-      case 'inputValue':
-        break;
       case 'posts':
-        renderPosts(state, posts, t);
+        renderPosts(this, elements, t);
         break;
       case 'feeds':
-        renderFeeds(state, feeds, t);
+        renderFeeds(state, elements, t);
         break;
       case 'message':
         renderMessage(state, elements, t);
         break;
-      case 'timerId':
-        break;
-      case 'urls':
+      case 'modal':
+        renderModal(this, elements);
         break;
       default:
+        renderPosts(this, elements, t);
         break;
     }
   }
 
-  return onChange(state, watch, { ignoreKeys: ['timerId'] });
+  return onChange(state, watch, { ignoreKeys: ['timerId', 'urls', 'inputValue'] });
 };

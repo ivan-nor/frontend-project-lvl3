60bf9a99592956182b20503a6db8cbe1961e6a09
Ivan Nor <ivan.norik@gmail.com>
2023-06-15 11:49:11 +0300

    fix names

--- src/js/watcher.js
+++ src/js/watcher.js
@@ -1,23 +1,23 @@
 import onChange from 'on-change';
 import {
-  renderFeeds, renderPosts, renderMessage, renderForm, renderModal,
+  renderFeeds, renderPosts, renderErrorMessage, renderForm, renderModal,
 } from './renders';
 
 export default (elements, state, t) => {
   function watch(path) { // value, previousValue
-    // console.log('PATH :>>', path, 'PREVVALUE :>>', previousValue, 'VALUE :>>', value);
+    // console.log('WATCHER PATH :>>', path, 'PREVVALUE :>>', previousValue, 'VALUE :>>', value);
     switch (path) {
-      case 'process':
-        renderForm(state, elements, t);
+      case 'processFeedAdding':
+        renderForm(this, elements, t);
         break;
       case 'posts':
         renderPosts(this, elements, t);
         break;
       case 'feeds':
-        renderFeeds(state, elements, t);
+        renderFeeds(this, elements, t);
         break;
-      case 'message':
-        renderMessage(state, elements, t);
+      case 'errorMessage':
+        renderErrorMessage(this, elements, t);
         break;
       case 'modal':
         renderModal(this, elements);

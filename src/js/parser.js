0a45728d295ff417c07e5ffcaa914be9144cc7da
Ivan Nor <ivan.norik@gmail.com>
2023-06-15 11:53:20 +0300

    add throwing error for return error & adding idies

--- src/js/parser.js
+++ src/js/parser.js
@@ -1,3 +1,5 @@
+import { uniqueId } from 'lodash';
+
 const isJsonString = (str) => {
   try {
     JSON.parse(str);
@@ -10,30 +12,42 @@ const isJsonString = (str) => {
 export default (str) => {
   const stringContainingXMLSource = isJsonString(str) ? JSON.parse(str) : str;
   const parserInstanse = new DOMParser();
-  const doc = parserInstanse.parseFromString(stringContainingXMLSource, 'application/xml');
-  // console.log('PARSER', doc);
-  const items = doc.querySelectorAll('item');
-  const channelTitle = doc.querySelector('channel > title');
-  const channelDescription = doc.querySelector('channel > description');
-  const channelLink = doc.querySelector('channel > link');
+  try {
+    const doc = parserInstanse.parseFromString(stringContainingXMLSource, 'application/xml');
+    // console.log(doc.querySelector('parsererror'));
+    if (doc.querySelector('parsererror')) {
+      throw new Error('formatError');
+    }
+    // console.log('PARSER', doc);
+    const items = doc.querySelectorAll('item');
+    const channelTitle = doc.querySelector('channel > title');
+    const channelDescription = doc.querySelector('channel > description');
+    const channelLink = doc.querySelector('channel > link');
 
-  const channel = {
-    channelTitle: channelTitle.textContent,
-    channelDescription: channelDescription.textContent,
-    channelPosts: [],
-    channelLink: channelLink.textContent,
-  };
+    const channel = {
+      channelTitle: channelTitle.textContent,
+      channelDescription: channelDescription.textContent,
+      channelPosts: [],
+      channelLink: channelLink.textContent,
+      channelId: uniqueId(),
+    };
 
-  items.forEach((item) => {
-    const title = item.querySelector('title').textContent;
-    const description = item.querySelector('description').textContent;
-    const link = item.querySelector('link').textContent;
+    items.forEach((item) => {
+      const title = item.querySelector('title').textContent;
+      const description = item.querySelector('description').textContent;
+      const link = item.querySelector('link').textContent;
 
-    channel.channelPosts.push({
-      title,
-      description,
-      link,
+      channel.channelPosts.push({
+        title,
+        description,
+        link,
+        id: uniqueId(),
+      });
     });
-  });
-  return channel;
+    return channel;
+  } catch (error) {
+    console.log('ERR in PARSER', error.message);
+
+    throw new Error('formatError');
+  }
 };
